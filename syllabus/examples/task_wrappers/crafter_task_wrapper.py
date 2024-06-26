1ec59482e372c26829f7e2e443a78135c036e62e
aKssup <118090347+aKssup@users.noreply.github.com>
2024-06-26 01:09:12 -0400

    basic task through seed structure

--- syllabus/examples/task_wrappers/crafter_task_wrapper.py
+++ syllabus/examples/task_wrappers/crafter_task_wrapper.py
@@ -7,6 +7,9 @@ class CrafterTaskWrapper(TaskWrapper):
     """
     This wrapper allows you to change the task of a Crafter environment.
     """
-    def __init__(self, env: gym.Env, seed=0):
-        super().
+    def __init__(self, env: gym.Env, area=(64, 64), view=(9, 9), size=(64, 64), seed=None):
+        super().__init__(env)
+        self.task_space = TaskSpace()
+        self.task = np.random.randint(0, 2**31 - 1) if seed is None else seed
+        
 
